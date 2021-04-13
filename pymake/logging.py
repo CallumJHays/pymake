12280cf33aa19c296f588901ef52d4767872a5e6
CallumJHays <callumjhays@gmail.com>
2021-04-13 10:20:58 +1000

    insert outer frame info for sh exec log

--- pymake/logging.py
+++ pymake/logging.py
@@ -1,4 +1,4 @@
-import logging
+from logging import *
 
 GREEN = "\x1b[32;21m"
 GREY = "\x1b[38;21m"
@@ -8,23 +8,33 @@ BOLD_RED = "\x1b[31;1m"
 RESET = "\x1b[0m"
 
 
-class CustomFormatter(logging.Formatter):
+class CustomFormatter(Formatter):
     """Logging Formatter to add colors and count warning / errors"""
 
-    def format(self, record: logging.LogRecord):
-        formatter = logging.Formatter({
-            logging.DEBUG: GREEN,
-            logging.INFO: GREY,
-            logging.WARNING: YELLOW,
-            logging.ERROR: RED,
-            logging.CRITICAL: BOLD_RED
+    def format(self, record: LogRecord):
+        frame = getattr(record, 'frame', None)
+        if frame:
+            # calling frame override requested
+            record.pathname = frame.filename
+            record.lineno = frame.lineno
+            record.funcName = frame.function
+
+        if record.pathname.startswith('/home/'):
+            record.pathname = f'~/{"/".join(record.pathname.split("/")[3:])}'
+
+        formatter = Formatter({
+            DEBUG: GREEN,
+            INFO: GREY,
+            WARNING: YELLOW,
+            ERROR: RED,
+            CRITICAL: BOLD_RED
         }[record.levelno]
             + "%(levelname)s [%(pathname)s:%(lineno)d]: %(message)s"
             + RESET)
         return formatter.format(record)
 
 
-logger = logging.getLogger('pymake')
-handler = logging.StreamHandler()
+logger = getLogger('pymake')
+handler = StreamHandler()
 handler.setFormatter(CustomFormatter())
 logger.addHandler(handler)
--- pymake/shell.py
+++ pymake/shell.py
@@ -4,6 +4,7 @@ from asyncio.subprocess import create_subprocess_shell, PIPE
 from subprocess import CalledProcessError
 from .utils import unindent
 from .logging import logger
+import inspect
 
 
 async def sh(
@@ -13,21 +14,26 @@ async def sh(
 ) -> Optional[bytes]:
     # TODO: print to terminal
     script = unindent(script)
+    if not silent:
+        frame = inspect.stack()[1]
+        logger.info(f'Running "{script}"', extra=dict(frame=frame))
+
     process = await create_subprocess_shell(
         unindent(script),
         cwd=cwd,
         stderr=PIPE, stdout=PIPE
     )
-    if not silent:
-        logger.info(f"Running {script}")
     stdout, stderr = await process.communicate()
     assert process.returncode is not None
     if process.returncode != 0:
-        raise ShellExecError(process.returncode, stderr.decode())
+        raise ShellExecError(process.returncode, script,
+                             stdout.decode(), stderr.decode())
 
     if not silent:
-        output = ('\n' + stdout.decode().strip()).replace('\n', '\n  >> ')
-        logger.info(output)
+        output = (
+            f'{script}\n{stdout.decode().strip()}'
+        ).replace('\n', '\n  >> ')
+        logger.info(output, extra=dict(frame=frame))  # type: ignore
     return stdout if any(stdout) else None
 
 
